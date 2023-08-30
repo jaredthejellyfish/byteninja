b03bcd840e87487bbe2f86f2d0222205749ba67a
Gerard Hernandez <ger.almenara@gmail.com>
2023-08-30 20:32:01 +0200

    fixed render methods in page-content.tsx

--- app/settings/page-content.tsx
+++ app/settings/page-content.tsx
@@ -5,7 +5,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 
-import { ExtendedUser } from '@/lib/types/types';
+import { UserWithSettings } from '@/lib/types/types';
 import { cn } from '@/lib/utils/cn';
 
 const GeneralSettingsPage = dynamic(() => import('./settings-pages/general'), {
@@ -19,7 +19,7 @@ const LoginConnectionsPage = dynamic(
   },
 );
 
-const BlankSettingsPage = (props: { user: ExtendedUser }) => {
+const BlankSettingsPage = (props: { user: UserWithSettings }) => {
   return <pre>{JSON.stringify(props.user, null, 2)}</pre>;
 };
 
@@ -42,7 +42,7 @@ const menuVariants = {
   },
 };
 
-const SettingsContent = ({ user }: { user: ExtendedUser }) => {
+const SettingsContent = ({ user }: { user: UserWithSettings }) => {
   const [activePage, setActivePage] = useState('Login Connections');
 
   const settingsPages = [
@@ -52,53 +52,51 @@ const SettingsContent = ({ user }: { user: ExtendedUser }) => {
     { name: 'Notifications', component: BlankSettingsPage },
   ];
 
-  const renderPage = (page: {
-    name: string;
-    component: React.ComponentType<{
-      user: ExtendedUser;
-    }>;
-  }) => {
-    if (page.name === activePage) {
-      const ActivePageComponent = page.component;
-      return <ActivePageComponent user={user} key={0} />;
-    }
-    return null;
-  };
-
   return (
     <div className="flex flex-row px-0 sm:px-10">
       <div className="flex flex-col sm:w-1/4 ml-[-12px] gap-0.5 justify-center sm:justify-start sm:items-start w-full">
-        {settingsPages.map((page) => (
-          <div className="pl-6 w-full sm:pl-0 xl:pl-[72px]" key={page.name}>
-            <div
-              className={cn(
-                'flex flex-row text-neutral-500 transition-all text-lg sm:min-w-[150px] hover:bg-neutral-500/30 py-2 px-3 rounded-sm sm:w-4/5 font-light cursor-pointer sm:text-base',
-                activePage === page.name &&
-                  'text-black dark:text-neutral-50 font-medium',
-              )}
-              onClick={() =>
-                setActivePage(page.name === activePage ? '' : page.name)
-              }
-            >
-              <span>{page.name}</span>
-            </div>
-            <Separator className="w-full h-[1px] bg-neutral-200 dark:bg-zinc-800 sm:hidden block" />
-            <div className="block sm:hidden overflow:hidden m-0">
-              <motion.div
-                variants={menuVariants}
-                initial="closed"
-                animate={activePage === page.name ? 'open' : 'closed'}
-                className="mt-5"
+        {settingsPages.map((page) => {
+          const ActivePageComponent = page.component;
+          return (
+            <div className="pl-6 w-full sm:pl-0 xl:pl-[72px]" key={page.name}>
+              <div
+                className={cn(
+                  'flex flex-row text-neutral-500 transition-all text-lg sm:min-w-[150px] hover:bg-neutral-500/30 py-2 px-3 rounded-sm sm:w-4/5 font-light cursor-pointer sm:text-base',
+                  activePage === page.name &&
+                    'text-black dark:text-neutral-50 font-medium',
+                )}
+                onClick={() =>
+                  setActivePage(page.name === activePage ? '' : page.name)
+                }
               >
-                {renderPage(page)}
-              </motion.div>
+                <span>{page.name}</span>
+              </div>
+              <Separator className="w-full h-[1px] bg-neutral-200 dark:bg-zinc-800 sm:hidden block" />
+              <div className="block sm:hidden overflow:hidden m-0">
+                <motion.div
+                  variants={menuVariants}
+                  initial="closed"
+                  animate={activePage === page.name ? 'open' : 'closed'}
+                  className="mt-5"
+                >
+                  {<ActivePageComponent user={user} key={0} />}
+                </motion.div>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div id="right" className="hidden sm:w-3/4 sm:block">
         <AnimatePresence>
-          {settingsPages.map((page) => renderPage(page))}
+          {settingsPages.map((page) => {
+            if (page.name === activePage) {
+              const ActivePageComponent = page.component;
+              return (
+                <ActivePageComponent user={user} key={0} />
+              );
+            }
+            return null;
+          })}
         </AnimatePresence>
       </div>
     </div>
