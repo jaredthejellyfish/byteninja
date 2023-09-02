9ea7f95ad876fb30068a559f5d42c7201a1a17f3
Gerard Hernandez <ger.almenara@gmail.com>
2023-09-02 14:20:23 +0200

    added session revalidation

--- components/providers.tsx
+++ components/providers.tsx
@@ -15,7 +15,11 @@ type Props = {
 const Providers = (props: Props) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider session={props.session}>
+      <SessionProvider
+        session={props.session}
+        refetchInterval={5 * 60}
+        refetchOnWindowFocus={true}
+      >
         {props.children}
       </SessionProvider>
       <ReactQueryDevtools initialIsOpen={false} />
