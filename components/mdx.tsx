c1ba16bca016423a8394bdba7abeebc69f42f5f6
Gerard Hernandez <ger.almenara@gmail.com>
2023-09-20 17:06:37 +0200

    added code theming

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -55,18 +55,44 @@ const components = {
     />
   ),
 
-  pre: ({ ...props }) => <Code theme={barnCatThemeSSR} {...props} />,
+  pre: ({ ...props }) => (
+    <Code
+      theme={{
+        dark: barnCatThemeSSR,
+        light: 'min-light',
+        // using a different CSS selector:
+        // lightSelector: '[data-theme="light"]',
+        lightSelector: 'html.light',
+      }}
+      {...props}
+    />
+  ),
 
   YoutubeEmbed,
 
   Collapsible,
 };
 
+function LoaingSpinner() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <div
+        className="inline-block h-20 w-20 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
+}
+
 export async function Mdx({ source }: MdxProps) {
   const Content = useMDXComponent(source);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoaingSpinner />}>
       <article className="prose prose-neutral dark:prose-invert max-w-none prose-h1:leading-normal px-7">
         {/* @ts-expect-error typing of a element is incompatible */}
         <Content components={components} />
